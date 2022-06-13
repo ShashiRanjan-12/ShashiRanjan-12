906a9f08b062c704829ff7d1b21983add0e2320c
ShashiRanjan-12 <103381437+ShashiRanjan-12@users.noreply.github.com>
2022-06-14 04:19:09 +0530

    Add files via upload
    ESP Project T&P Module

---
+++ PLACEMENT.sql
@@ -0,0 +1,24 @@
+create table Placement (
+
+Student_Name varchar NOT NULL,
+student_FatherName varchar NOT NULL,
+Student_MotherName varchar  NOT NULL,
+Student_DOB datetime NOT NULL,
+Student_contactdetails bigint NOT NULL,
+Student_Religion varchar NOT NULL,
+Student_BloodGroup varchar NOT NULL,
+Student_Address varchar NOT NULL,
+Student_Department varchar NOT NULL,
+Student_DepartmentID bigint NOT NULL,
+student_SemesterDetails float NOT NULL,
+student_Skills varchar NOT NULL,
+student_InternshipCompany varchar NOT NULL,
+student_InternshipDuration bigint NOT NULL,
+student_Interntechnology varchar NOT NULL,
+student_TrainingCompany varchar NOT NULL,
+student_TrainingDuration bigint NOT NULL,
+student_trainingtechnology varchar NOT NULL,
+student_ExtraActivityDetails varchar NOT NULL,
+Placement_companies varchar  NOT NULL
+
+);
