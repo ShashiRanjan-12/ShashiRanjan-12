3a1339859b9b3a9856c7d286cfd077285f39ae35
ShashiRanjan-12 <103381437+ShashiRanjan-12@users.noreply.github.com>
2022-06-14 04:20:17 +0530

    Add files via upload
    INTERNSHIP SUB MODULE IN T&P MODULE

---
+++ INTERNSHIP.sql
@@ -0,0 +1,24 @@
+create table Internship(
+Intern_Name varchar NOT NULL,
+Intern_ID varchar(20) NOT NULL,
+Intern_Department varchar(50) NOT NULL,
+Intern_Semesterdetails bigint ,
+Intern_Gender varchar(8) NOT NULL,
+Intern_DOB datetime NOT NULL,
+Intern_ContactDetails int NOT NULL,
+Intern_mail varchar (40) NOT NULL,
+Intership_code varchar NOT NULL,
+Intership_companyname  varchar NOT NULL,
+Inter_role varchar  NOT NULL,
+Intership_startdate datetime NOT NULL,
+Internship_lastdate datetime NOT NULL,
+Intership_mode varchar NOT NULL,
+Intership_Duration int NOT NULL,
+Internship_certificate varchar NOT NULL,
+Internship_cerificateID bigint NOT NULL,
+
+Course_ID varchar NOT NULL,
+Course_Name varchar(50) NOT NULL ,
+
+
+);
